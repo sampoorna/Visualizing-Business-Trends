4b97aa0028a64a398f7a274edbf7a6657859a455
sampoorna <sampoorna.biswas01@gmail.com>
2017-10-30 11:26:58 -0700

    Merged changes

--- milestones.py
+++ milestones.py
@@ -49,7 +49,7 @@ while (invalid_response):
 	else:
 		invalid_response = False
 
-# Take file names as command line input
+# Take file name as command line input
 invalid_response = True
 
 while (invalid_response):
@@ -66,6 +66,7 @@ while (invalid_response):
 
 invalid_response = True
 
+# Take second file name as command line input
 if overlay_events_flag == 'y':
 	while (invalid_response):
 		filename_milestones = raw_input("Enter the file name (case sensitive) that contains milestones/notable events information (include the extension, for example: events.csv): ")
@@ -77,8 +78,8 @@ if overlay_events_flag == 'y':
 			event_data_headers = next(event_data, None)  # Read the headers
 			invalid_response = False
 		except:
-			print "Could not find file. Please ensure that file exists and file name was correctly input - remember, the name is case sensitive!"	
-
+			print "Could not find file. Please ensure that file exists and file name was correctly input - remember, the name is case sensitive!"
+			
 invalid_response = True
 
 while (invalid_response):
@@ -100,7 +101,7 @@ while (invalid_response):
 		invalid_response = False
 	except:
 		print "Input does not appear to be a date :S Please try again!"
-
+		
 # Ask user which metric to plot on which axis
 print "Found following data headers: "
 for i in range(len(data_headers)):
@@ -108,15 +109,20 @@ for i in range(len(data_headers)):
 
 date_column_index = input("Choose column that contains X-axis values (dates): ") - 1
 first_axis_columns = raw_input("Choose column(s) that contain(s) first Y-axis values (separate numbers by space): ")
-second_axis_columns = raw_input("Choose column(s) that contain(s) second Y-axis values (separate numbers by space): ")
-	
+first_axis_columns = [int(i) - 1 for i in first_axis_columns.split(' ')]
+second_axis_columns = raw_input("Choose column(s) that contain(s) second Y-axis values (separate numbers by space). Type 0 if all columns are to be plotted on the first axis only: ")
+second_axis_columns = [int(i) - 1 for i in second_axis_columns.split(' ')]
+
+if second_axis_columns[0] == -1:
+	first_axis_only = True
+	columns_to_plot = set().union(first_axis_columns, second_axis_columns)
+else:
+	first_axis_only = False
+	columns_to_plot = first_axis_columns
+
 date_range_start = []
 date_range_end = []
 measures_to_plot = [[] for i in range(len(data_headers) - 1)] # Initialize array to appropriate size
-net_mrr = []
-uninstalls = []
-installs = []
-data_indexes = {}
 data_index_second_axis = -1
 y_label = 'Number of '
 
@@ -128,35 +134,34 @@ data_legends = data_headers[1:]
 for line in stats:
 	# Parse date fields as dates
 	rs = parse(line[date_column_index].split('-')[0].strip())
-	#re = datetime.strptime(line[0].split('-')[1].strip(), '%B %d, %Y')
 	re = parse(line[date_column_index].split('-')[1].strip())
-	#re = re.replace(year = 2017)
-	date_range_start.append(rs)
-	date_range_end.append(re)
 	
-	# Read data fields line by line
-	for metric in range(len(data_headers) - 1):
+	if (rs >= plot_date_start):
+		date_range_start.append(rs)
+	if (re <= plot_date_end):
+		date_range_end.append(re)
+	
+	# Iterate over all columns and read in data fields
+	for metric in columns_to_plot:
 		value = line[metric + 1]
-		if data_headers[metric + 1] == 'Net MRR':
-			data_index_second_axis = metric
-		measures_to_plot[metric].append(float(value.replace(',', '')))
+		measures_to_plot[metric].append(float(value.replace(',', ''))) # Strip out commas from numeric values and convert to float
 		
 fdts = dates.date2num(date_range_end) # Convert dates to numbers
 hfmt = dates.DateFormatter('%b %d') # Format dates
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax2 = ax.twinx()
 
+if not first_axis_only:
+	ax2 = ax.twinx()
 
 for metric in range(len(data_headers) - 1):
 	result = curve_fit(fdts, measures_to_plot[metric]) # Fit curve to data points to estimate trend
-	if metric == data_index_second_axis:
-		ax.plot(fdts, measures_to_plot[metric], label = data_legends[metric], marker = markers[metric], color = colours[metric], linewidth = 1) # Plot raw data points
-		ax.plot(result[0], result[1], label = 'Estimated ' + data_legends[metric], linestyle = '--', color = colours[metric], linewidth = 3) # Plot trend curve
-		#print result[1]
-	else:
+	if not first_axis_only and metric in second_axis_columns:
 		ax2.plot(fdts, measures_to_plot[metric], label = data_legends[metric], marker = markers[metric], color = colours[metric], linewidth = 1) # Plot raw data points
 		ax2.plot(result[0], result[1], label = 'Estimated ' + data_legends[metric], linestyle = '--', color = colours[metric], linewidth = 3) # Plot trend curve
+	else:
+		ax.plot(fdts, measures_to_plot[metric], label = data_legends[metric], marker = markers[metric], color = colours[metric], linewidth = 1) # Plot raw data points
+		ax.plot(result[0], result[1], label = 'Estimated ' + data_legends[metric], linestyle = '--', color = colours[metric], linewidth = 3) # Plot trend curve
 		y_label += data_legends[metric].lower() + ', ' # Construct label for Y axis
 
 ### Graph legends etc
@@ -164,22 +169,22 @@ ax.set_xlabel('Week ending in') # Setting X axis label
 plt.xticks(fdts) # Place X axis ticks for every week
 ax.xaxis.set_major_formatter(hfmt) # Format X tick labels
 ax.set_ylabel('Dollar amounts') # Setting Y axis label: remove the last comma
-ax2.set_ylabel(y_label[:-2] + " (weekly)") # Setting the second Y axis label
+
+if not first_axis_only:
+	ax2.set_ylabel(y_label[:-2] + " (weekly)") # Setting the second Y axis label
+
 # Place legends on the graph
 ax.legend(loc = 6)
-ax2.legend(loc = 2)
 ax.set_ylim(ymin=0)
-ax2.set_ylim(ymin=0)
+
+if not first_axis_only:
+	ax2.legend(loc = 2)
+	ax2.set_ylim(ymin=0)
 
 if overlay_events_flag == 'y': # If overlaying events
 
-	# Opening and reading file
-	f = open(filename_milestones, 'rU')
-	stats = csv.reader(f, delimiter=',')
-	headers = next(stats, None)  # Skip the headers
-
-	range_start_ms = []
-	range_end_ms = []
+	date_range_start_events = []
+	date_range_end_events = []
 	milestones = []
 	classes = []
 	y = []
@@ -190,12 +195,12 @@ if overlay_events_flag == 'y': # If overlaying events
 	indexes = {}
 	
 	# To use header names to index
-	for h in range(len(headers)):
-		indexes[headers[h]] = h
+	for h in range(len(event_data_headers)):
+		indexes[event_data_headers[h]] = h
 	
 	# Sort events by degree of severity
-	stats = list(stats)
-	stats.sort(key=lambda x: int(x[indexes['Severity']]))
+	event_data = list(event_data)
+	event_data.sort(key=lambda x: int(x[indexes['Severity']]))
 
 	# Parse and process data
 	for line in stats:
@@ -206,8 +211,11 @@ if overlay_events_flag == 'y': # If overlaying events
 			re = rs
 		else:
 			re = parse(line[indexes['End Date']].strip()) 
-		range_start_ms.append(rs)
-		range_end_ms.append(re)	
+		
+		if (rs >= plot_date_start):
+			date_range_start_events.append(rs)
+		if (re <= plot_date_end):
+			date_range_end_events.append(re)
 		
 		# Assign marker colours according to degree of severity
 		classes.append(line[indexes['Severity']])
